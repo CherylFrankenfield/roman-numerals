aec1a8de6e01bc8833935afbe0c3c1f3d49dac8a
Cheryl Frankenfield and Tyler Bussanich <tyler@>
2017-10-26 14:43:04 -0700

    Add single thru quad blank strings

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,13 @@
 // Business or Backend Logic
-var romans = ["I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"]
-var numbers = ["1", "4", "5", "9", "10", "40", "50", "90", "100", "400", "500", "900", "1000"]
 
+// For reference only:
+// var romans = ["I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"]
+// var numbers = ["1", "4", "5", "9", "10", "40", "50", "90", "100", "400", "500", "900", "1000"]
+var singles = ["I", "IV", "V", "IX"]
+var tens = ["X", "XL", "L", "XC"]
+var hundreds = ["C", "CD", "D", "CM"]
+var thousands = ["M"]
+var numberArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
 // User Interface or Frontend Logic Here
 $(document).ready(function() {
@@ -10,33 +16,37 @@ $(document).ready(function() {
     var userInput = $("input#digits").val();
 
 
-    var single = $("#singleInput").val();
-    var double = $("#doubleInput").val();
-    var triple = $("#tripleInput").val();
-    var quad = $("#quadInput").val();
+    var single = "";
+    var double = "";
+    var triple = "";
+    var quad = "";
+    // var double = $("#doubleInput").val();
+    // var triple = $("#tripleInput").val();
+    // var quad = $("#quadInput").val();
 
     if (userInput.length === 1) {
-      userInput = single
-      alert("single");
+        for (index = 0; index <= numberArray.length; index++) {
+          if (numberArray[i] === userInput) {
+
+          }
+          return numberArray
+          // alert(numberArray);
+        }
+      // alert("single");
     } else if (userInput.length === 2) {
       userInput = double
-      alert("double");
+      // alert("double");
     } else if (userInput.length === 3) {
       userInput = triple
-      alert("triple");
+      // alert("triple");
     } else {
       (userInput.length === 4)
       userInput = quad
-      alert("quad");
+      // alert("quad");
     }
 
-    // for (var index = 0; index < userInput.length; index +-1) {
-    //   // if (userInput.charAt() = 1) {
-    //   }
-    // }
 
 
-
-  $("#result").append("<p>" + romanResult + "</p>");
+  // $("#result").append("<p>" + romanResult + "</p>");
 });
 });
